047879df3043d692b346950029386f2bc2d9c8c3
maria.th.pantsiou@gmail.com <maria.pantsiou@gmail.com>
2020-02-17 10:13:26 +0000

    Add hover tool

--- bokeh_2nd_example.py
+++ bokeh_2nd_example.py
@@ -5,7 +5,9 @@ Created on Fri Feb 14 16:00:56 2020
 
 @author: punchubu1804
 """
-from bokeh.plotting import figure, output_file, show, ColumnDataSource
+from bokeh.plotting import figure, output_file, show, save, ColumnDataSource
+from bokeh.models.tools import HoverTool
+from bokeh.models.glyphs import ImageURL
 import pandas as pd
 
 
@@ -23,15 +25,15 @@ def cars_plot(df):
     """    
     
     # create ColumnDataSource from dataframe
-    source = ColumnDataSource(pd)   
+    source = ColumnDataSource(df)   
     
     
     # car list
-    car_list = source.data['Car'].to_list()
+    car_list = source.data['Car'].tolist()
     
     # figure
     p = figure(
-        y_range=car,
+        y_range=car_list,
         plot_width=800,
         plot_height=600,
         title='Cars Horsepower',
@@ -39,7 +41,6 @@ def cars_plot(df):
         y_axis_label='Cars',
         # set the tools
         tools='pan, box_select, zoom_in, zoom_out, save, reset',
-        source=source
         )
     
     # use bokeh horizontal bar glyphs
@@ -49,8 +50,35 @@ def cars_plot(df):
         left=0,
         height=0.4,
         color='navy',
-        fill_alpha=0.5 # opacity
+        fill_alpha=0.5, # opacity
+        source=source
         )
+    
+    # Add tooltips with ColumnDataSoure
+    # mouse hover over
+    hover = HoverTool()
+    hover2 = HoverTool()
+    # dict of tuples (name, field) or a raw html text in """ """
+    # an image will be shown when hove over
+    # access the data with @ (field names associated with the ColumnDataSource columns), <string> defines important text, 
+    hover.tooltips = """
+    <div>
+      <h3>@Car</h3>
+      <div><strong>Price: </strong>@Price</div>
+      <div><strong>HP: </strong>@Horsepower</div>
+      <div><img src="@Image" alt="Car Image" width="200" /></div>
+    </div>
+    """
+    p.add_tools(hover)
+    
+    # second hover tool
+    # hover2.tooltips=[
+    #     ('Image', '@Image'),
+    #     ('Horsepower', '@Horsepower'),
+    #     ('Price', '@Price'),
+    #     ]
+    # p.add_tools(hover2)
+    
     
     return p
 
